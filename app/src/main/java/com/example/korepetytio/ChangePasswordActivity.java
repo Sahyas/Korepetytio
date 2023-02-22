4da16e5851d12d934bfc5e3c88ff5470c83a0f9d
Radek <236607@edu.p.lodz.pl>
2023-02-22 15:40:39 +0100

    update password

--- app/src/main/java/com/example/korepetytio/ChangePasswordActivity.java
+++ app/src/main/java/com/example/korepetytio/ChangePasswordActivity.java
@@ -1,23 +1,19 @@
 package com.example.korepetytio;
 
-import static android.content.ContentValues.TAG;
-
 import static com.example.korepetytio.LoginActivity.client;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnFailureListener;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.korepetytio.client.ClientRole;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
@@ -25,9 +21,6 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class ChangePasswordActivity extends AppCompatActivity {
     private Button button13;
@@ -75,8 +68,15 @@ public class ChangePasswordActivity extends AppCompatActivity {
     }
 
     public void changePassword(String newPassword, String email) {
+        String collection;
+        if(client.getRole()== ClientRole.STUDENT){
+            collection = "students";
+        }
+        else {
+            collection = "teachers";
+        }
         FirebaseFirestore db = FirebaseFirestore.getInstance();
-        CollectionReference studentsRef = db.collection("students");
+        CollectionReference studentsRef = db.collection(collection);
         Query query = studentsRef.whereEqualTo("email", email);
         query.get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
             @Override
